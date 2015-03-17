241af665535aaf0dae8c2b0eb036977a872a2ec1
Tyler Weir <tyler.weir@gmail.com>
2015-03-17 16:12:34 -0400

    octagon keymap

--- keyboard/octagon/keymap_winkey.h
+++ keyboard/octagon/keymap_winkey.h
@@ -1,5 +1,7 @@
 #include "backlight.h"
 
+//LALT,LALT,LALT,                  SPC,                FN9,FN0,LEFT,DOWN,RGHT),
+
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(\
         ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PGUP,PAUS, \
@@ -7,7 +9,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,HOME,  \
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,PGDN,ENT, PGUP, \
         LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,UP,  PGDN,  \
-        LGUI,LGUI,LALT,                  SPC,                FN9,FN0,LEFT,DOWN,RGHT),
+        LALT, LGUI, 3,             SPC,                FN9,FN0,LEFT,DOWN,RGHT),
     KEYMAP(\
            SLEP,MYCM,WSCH,WHOM,MAIL,MUTE,VOLD,VOLU,MSEL,MSTP,MPLY,MPRV,MNXT,TRNS,TRNS, \
            FN1, FN2, FN3, FN4, FN5, FN6, FN7, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
